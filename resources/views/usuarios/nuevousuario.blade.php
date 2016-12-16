7c4c7e04b15786b31ae462094a638c40c5867416
Luis Ugueto <luis@oceandg.com>
2016-12-15 20:31:35 -0430

    Editando Pantallas

--- resources/views/usuarios/nuevousuario.blade.php
+++ resources/views/usuarios/nuevousuario.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('htmlheader_title')
-    Home
+    Nuevo Usuario
 @endsection
 
 
--- resources/views/usuarios/usuario.blade.php
+++ resources/views/usuarios/usuario.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('htmlheader_title')
-    Home
+    Usuarios
 @endsection
 
 
