17779575d334cd36c9826868429eb55a070b6242
Luis <blink242@outlook.com>
2016-12-18 12:17:32 -0400

    Arreglando Pantallas

--- resources/views/usuarios/usuario.blade.php
+++ resources/views/usuarios/usuario.blade.php
@@ -8,6 +8,7 @@
 
 @section('main-content')                    
 <div class="col-md-12">
+
     @if(Session::has('message'))
         <div class="alert alert-success alert-dismissible" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
@@ -24,7 +25,7 @@
     </div>
     <section class="content">
         <div class="row">
-            <div class="col-md-14">
+            <div class="col-md-12">
                 <div class="box">
                     <div class="box-header">
                         <h3 class="box-title">Tabla</h3>
@@ -47,12 +48,15 @@
                                         <td>{{$usuario->name}}</td>
                                         <td>{{$usuario->email}}</td>
                                         <td>{{$usuario->roles->nombre }}</td>
-                                        <td>{!!link_to_route('usuarios.edit', $title = 'Editar', $parameters = $usuario->id, $attributes = ['class'=>'btn btn-primary'])!!}
+                                        <td>{!!link_to_route('usuarios.edit', $title = 'Editar', $parameters = $usuario->id, $attributes = ['class'=>'btn btn-primary'])!!}</td>
                                     </tr>
                                 @endforeach
                             </tbody>
                         </table>
-    
-                        </div>
                     </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
 @stop
