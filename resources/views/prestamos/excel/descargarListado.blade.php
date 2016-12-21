abb295c5ac66729ae9606362cc74af0a5ff52d88
Luis <blink242@outlook.com>
2016-12-21 12:17:31 -0400

    Excel Editado

--- resources/views/prestamos/excel/descargarListado.blade.php
+++ resources/views/prestamos/excel/descargarListado.blade.php
@@ -1,3 +1,27 @@
+<table>
+    <tr>
+        <td colspan="7" style="text-align:center">
+            <b>ESCUELA DE EDUCACIÓN BÁSICA PARTICULAR</b>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="7" style="text-align:center">
+            <b><h3>"MARIA MONTESSORI"</h3></b>
+        </td>
+    </tr>        
+    <tr>
+        <td colspan="7" style="text-align:center">
+            FLORIDA NORTE COOP UNIDOS SOMOS MAS MZ 385 SI 22
+        </td>
+    </tr>
+    
+    <tr>
+        <td colspan="7" style="text-align:center">
+            LISTADO DE PRESTAMOS Y ANTICIPOS
+        </td>
+    </tr>
+</table>
+
  <table id="example1" class="table table-bordered table-hover">
                             <thead>
                                 <tr>
