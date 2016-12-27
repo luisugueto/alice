66e8a6d4069f2b2638ce38a4868fb58f50f3a5b3
Luis <blink242@outlook.com>
2016-12-27 10:44:15 -0400

    Footer corregido Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,13 @@
 @section('contentheader_description', 'Inicio')
 
 @section('main-content')
-
-	<div class="container spark-screen">
-	@if(Session::has('message-error'))
+<div class="col-md-12">
+   
+    
+    @include('alerts.errors')
+    
+    <section class="content">
+     @if(Session::has('message-error'))
         <div class="alert alert-success alert-dismissible" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             <ul>
@@ -14,26 +18,18 @@
             </ul>
         </div>
     @endif
-    @if(Session::has('message'))
-        <div class="alert alert-success alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <ul>
-                {{Session::get('message')}}
-            </ul>
-        </div>
-    @endif
-		<div class="row">
-			<div class="col-md-10 col-md-offset-1">
-				<div class="panel panel-default">
-					<div class="panel-heading">Bienvenido</div>
-
-					<div class="panel-body">
-					 	{{Session::get('message')}}
-						{{ trans('adminlte_lang::message.logged') }}
-					</div>
-					
-				</div>
-			</div>
+    <div class="container spark-screen">
+    	<div class="row">
+    	    <div class="col-md-10 col-md-offset-1">
+    			<div class="panel panel-default">
+    				<div class="panel-heading">Bienvenido</div>
+    	       			<div class="panel-body">
+    					 	{{Session::get('message')}}
+    						{{ trans('adminlte_lang::message.logged') }}
+    					</div>
+				    </div>
+		    </div>
 		</div>
 	</div>
+
 @endsection
