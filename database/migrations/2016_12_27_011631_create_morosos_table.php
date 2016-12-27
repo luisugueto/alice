2bbe967cf1759cb92cdd164b58962b95ef5f16f7
Jesús Eduardo <mtr_1101@hotmail.com>
2016-12-27 01:29:26 -0430

    Morosos

---
+++ database/migrations/2016_12_27_011631_create_morosos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMorososTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('morosos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_estudiante')->unsigned();
+            $table->foreign('id_estudiante')->references('id')->on('datos_generales_estudiante')->onDelete('Cascade');
+            $table->date('fecha');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('morosos');
+    }
+}
